45203f435b1d56d2d8760f239b564e18a928759a
Pascal-Flores <pascal.flores@etu.univ-amu.fr>
2021-12-02 15:16:29 +0100

    suppression de code généré inutile et déclaration de la fonction JSONRequestExecuter dans JSONController

--- Springboot_REST_API/src/main/java/com/example/demo/MyOwnRuntimeException.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo;
-
-public class MyOwnRuntimeException extends RuntimeException{
-    public MyOwnRuntimeException(String message) {
-        super(message);
-    }
-}
--- Springboot_REST_API/src/main/java/falsigram/Falsigram.java
+++ Springboot_REST_API/src/main/java/falsigram/Falsigram.java
@@ -1,18 +1,14 @@
-package com.example.demo;
+package falsigram;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class DemoApplication {
+public class Falsigram {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(Falsigram.class, args);
     }
 
-    @Bean
-    public Service service() {
-        return new Service();
-    }
 }
