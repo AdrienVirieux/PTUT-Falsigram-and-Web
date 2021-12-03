b01df81f7d725fb5b8cb1e565b6f03f73b5c8d5e
Pascal-Flores <pascal.flores@etu.univ-amu.fr>
2021-12-03 17:25:50 +0100

    implémentation du constructeur

--- Springboot_REST_API/src/main/java/falsigram/text/core/Sentence.java
+++ Springboot_REST_API/src/main/java/falsigram/text/core/Sentence.java
@@ -15,12 +15,20 @@ public class Sentence {
     private char punctuation;
     private List<List<Character>> content;
 
-
     /**
      * Default constructor
      */
-    public Sentence() {
+    public Sentence(String content) {
+        List<Character> tmp = new ArrayList<Character>();
+        for (int i = 0; i < content.length()-1; ++i){
+            if (content.charAt(i) != ' '){
+                tmp.add(content.charAt(i));
+            }
+            else {
+                this.content.add(tmp);
+                tmp.clear();
+            }
+        }
+        this.punctuation = content.charAt(content.length()-1);
     }
-
-
 }
