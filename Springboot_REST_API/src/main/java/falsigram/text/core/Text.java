f2a9419ed9f5f1fb12128d9ea3009cbc2535da7d
Pascal-Flores <pascal.flores@etu.univ-amu.fr>
2021-12-03 16:58:19 +0100

    implémentation du constructeur

--- Springboot_REST_API/src/main/java/falsigram/text/core/Text.java
+++ Springboot_REST_API/src/main/java/falsigram/text/core/Text.java
@@ -9,12 +9,21 @@
 package falsigram.text.core;
 
 
+import falsigram.text.utils.Replacer;
+
+import java.util.List;
+
 public class Text {
+
+    private List<Sentence> content;
 
     /**
      * Default constructor
      */
-    public Text() {
+    public Text(String content) {
+        for (String s : content.split("[.!?:]")){
+            this.content.add(new Sentence(s));
+        }
     }
 
 
