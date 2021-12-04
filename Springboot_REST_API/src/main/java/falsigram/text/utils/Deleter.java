4fd7883208ad5742f08fdb943aadecf2411e377e
Patate_Douce <86917957+AdrienVirieux2@users.noreply.github.com>
2021-12-04 03:04:25 +0100

    commit initial des fonctions de deletion

--- Springboot_REST_API/src/main/java/falsigram/text/utils/Deleter.java
+++ Springboot_REST_API/src/main/java/falsigram/text/utils/Deleter.java
@@ -8,8 +8,10 @@
 
 package falsigram.text.utils;
 
+import falsigram.text.core.Sentence;
 import falsigram.text.core.Text;
 
+import java.text.Normalizer;
 import java.util.*;
 
 
@@ -21,54 +23,141 @@ public class Deleter {
     public Deleter() {
     }
 
+    /*
+    private static List<Character> deleteChar(List<Character> l, int index) {
+        l.remove(index);
+        return l;
+    }
+    */
+
     /**
-     * @param List l 
-     * @param int index 
-     * @return  List<Character>
+     * @param text Text
+     * @param occurrence float
+     * @return Text
      */
-    private List<Character> deleteChar(List<Character> l, int index) {
-        // TODO implement here
-        return null;
+    public static Text deleteLetters(Text text, float occurrence) {
+        int charIndex;
+
+        for (Sentence sentence : text) {
+            for (List<Character> word : sentence) {
+                charIndex = 0;
+                for (Character c : word) {
+
+                    if (occurrence != 1) {
+                        /* On créer un nombre aléatoire entre 0 et 1 */
+                        float randNbr = new Random().nextFloat();
+                        if (occurrence > randNbr) {
+                            word.remove(charIndex);
+                        }
+                    }
+                    else
+                        word.remove(charIndex);
+                    charIndex += 1;
+
+                }
+            }
+        }
+
+        return text;
+    }
+
+
+    /**
+     * @param text Text
+     * @param occurrence float
+     * @return Text
+     */
+    public static Text deleteSpaces(Text text, float occurrence) {
+        int wordIndex;
+
+        for (Sentence sentence : text) {
+            wordIndex = 0;
+            for (List<Character> word : sentence) {
+                if (word.equals(sentence.getContent().get(0))) continue;
+
+                if (occurrence != 1) {
+                    /* On créer un nombre aléatoire entre 0 et 1 */
+                    float randNbr = new Random().nextFloat();
+                    if (occurrence > randNbr) {
+                        for (Character c : word)
+                            sentence.getContent().get(wordIndex).add(c);
+                        word.clear();
+                    }
+                }
+                else {
+                    for (Character c : word)
+                        sentence.getContent().get(wordIndex).add(c);
+                    word.clear();
+                }
+                wordIndex += 1;
+
+            }
+        }
+
+        return text;
+    }
+
+
+    /**
+     * @param text Text
+     * @param occurrence float
+     * @return Text
+     */
+    public static Text deleteAccents(Text text, float occurrence) {
+        for (Sentence sentence : text) {
+            for (List<Character> word : sentence) {
+                for (Character c : word) {
+                    // TODO faire en sorte de ne prendre que les caracteres accentués
+
+                    if (occurrence != 1) {
+                        /* On créer un nombre aléatoire entre 0 et 1 */
+                        float randNbr = new Random().nextFloat();
+                        if (occurrence > randNbr) {
+                            String tmp = Normalizer.normalize(String.valueOf(c), Normalizer.Form.NFD).replaceAll("[^\\p{ASCII}]", "");
+                            c = tmp.charAt(0);
+                        }
+                    }
+                    else {
+                        String tmp = Normalizer.normalize(String.valueOf(c), Normalizer.Form.NFD).replaceAll("[^\\p{ASCII}]", "");
+                        c = tmp.charAt(0);
+                    }
+
+                }
+            }
+        }
+
+        return text;
     }
 
     /**
-     * @param Text t 
-     * @param float occurrence 
+     * @param text Text
+     * @param occurrence float
      * @return Text
      */
-    public Text deleterLetters(Text t, float occurrence) {
-        // TODO implement here
-        return null;
-    }
+    public static Text deletePunctuations(Text text, float occurrence) {
+        int sentenceIndex = 0;
 
-    /**
-     * @param Text t 
-     * @param float occurrence 
-     * @return Text
-     */
-    public Text deleterSpaces(Text t, float occurrence) {
-        // TODO implement here
-        return null;
-    }
+        for (Sentence sentence : text) {
+            if (sentence.equals(text.getContent().get(0))) continue;
 
-    /**
-     * @param Text t 
-     * @param float occurrence 
-     * @return Text
-     */
-    public Text deleterAccents(Text t, float occurrence) {
-        // TODO implement here
-        return null;
-    }
+            if (occurrence != 1) {
+                /* On créer un nombre aléatoire entre 0 et 1 */
+                float randNbr = new Random().nextFloat();
+                if (occurrence > randNbr) {
+                    for (List<Character> word : sentence)
+                        text.getContent().get(sentenceIndex).getContent().add(word);
+                    sentence.remove();
+                }
+            }
+            else {
+                for (List<Character> word : sentence)
+                    text.getContent().get(sentenceIndex).getContent().add(word);
+                sentence.remove();
+            }
+            sentenceIndex += 1;
+        }
 
-    /**
-     * @param Text t 
-     * @param float occurrence 
-     * @return Text
-     */
-    public Text deleterPunctuations(Text t, float occurrence) {
-        // TODO implement here
-        return null;
+        return text;
     }
 
 }
