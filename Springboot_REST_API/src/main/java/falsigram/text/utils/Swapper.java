355e2f8a819641a1dab909b71e5017185108a9e4
Pascal-Flores <pascal.flores@etu.univ-amu.fr>
2021-12-14 11:32:37 +0100

    optimisation

--- Springboot_REST_API/src/main/java/falsigram/text/utils/Swapper.java
+++ Springboot_REST_API/src/main/java/falsigram/text/utils/Swapper.java
@@ -16,139 +16,114 @@ import static java.util.Collections.swap;
 
 public class Swapper {
 
-    /**
-     * Default constructor
-     */
-    public Swapper() {
-    }
-
+    private static final Random randomGenerator = new Random();
 
     /**
-     * @param Text t
-     * @param float proba
+     * @param Text text
+     * @param float occurrence
      * @return Text
      */
-    public Text swapCharsInWord(Text t, float proba) {
-        Random r = new Random();    //On crée un nombre aléatoire entre 0 et 1
-        if (proba != 1) {           //On vérifie que la proba ne bloque pas la suite de la fonction
-            for (Sentence s : t) {
-                for (List<Character> word : s) {
-                    for (int i = 0; i < word.size(); ++i) {
-                        if (proba > r.nextFloat()) {
-                            swap(word, i, r.nextInt(word.size() - 1));
+    public static void swapLettersInWord(Text text, float occurrence) {
+        if (occurrence != 1) {           //On vérifie que la occurrence ne bloque pas la suite de la fonction
+            for (Sentence sentence : text.getContent()) {
+                for (List<Character> word : sentence.getContent()) {
+                    for (int i = 0; i < word.size()-1; ++i) {
+                        if (randomGenerator.nextFloat() < occurrence) {
+                            swap(word, i, randomGenerator.nextInt(word.size() - 1));
                         }
                     }
                 }
             }
-            return t;
         }
         else
         {
-            for (Sentence s : t) {
-                for (List<Character> word : s) {
-                    for (int i = 0; i < word.size(); ++i) {
-                        swap(word, i, r.nextInt(word.size() - 1));
+            for (Sentence sentence : text.getContent()) {
+                for (List<Character> word : sentence.getContent()) {
+                    for (int i = 0; i < word.size()-1; ++i) {
+                        swap(word, i, randomGenerator.nextInt(word.size() - 1));
                     }
                 }
             }
-            return t;
         }
     }
 
 
     /**
-     * @param Text t
-     * @param float proba
+     * @param Text text
+     * @param float occurrence
      * @return Text
      */
-    public Text swapContiguousLettersInWord(Text t, float proba) {
-        if (proba != 1) {   //On vérifie que la proba ne bloque pas la suite de la fonction
-            Random r = new Random();    //On crée un nombre aléatoire entre 0 et 1
-            for (Sentence s : t) {
-                for (List<Character> word : s) {
+    public static void swapContiguousLettersInWord(Text text, float occurrence) {
+        if (occurrence != 1) {   //On vérifie que la occurrence ne bloque pas la suite de la fonction
+            for (Sentence sentence : text.getContent()) {
+                for (List<Character> word : sentence.getContent()) {
                     for (int i = 0; i < word.size() - 1; ++i) {
-                        if (proba > r.nextFloat()) {
-                            swap(word, i, i + 1);
+                        if (randomGenerator.nextFloat() < occurrence) {
+                            Collections.swap(word, i, i + 1);
                         }
                     }
                 }
             }
-            return t;
         }
         else
         {
-            for (Sentence s : t) {
-                for (List<Character> word : s) {
+            for (Sentence sentence : text.getContent()) {
+                for (List<Character> word : sentence.getContent()) {
                     for (int i = 0; i < word.size() - 1; ++i) {
                         swap(word, i, i + 1);
                     }
                 }
             }
-            return t;
         }
     }
 
 
     /**
-     * @param Text t
-     * @param float proba
+     * @param Text text
+     * @param float occurrence
      * @return Text
      */
-    public Text swapWordsInSentence(Text t, float proba) {
-        Random r = new Random();    //On crée un nombre aléatoire entre 0 et 1
-        if (proba != 1) {           //On vérifie que la proba ne bloque pas la suite de la fonction
-            for (Sentence s : t) {
-                for (int i = 0; i < s.getContent().size(); ++i) {
-                    if (proba > r.nextFloat()) {
-                        List<List<Character>> temp = s.getContent();
-                        swap(temp, i, r.nextInt(temp.size() - 1));
-                        s.setContent(temp);
+    public static void swapWordsInSentence(Text text, float occurrence) {
+        if (occurrence != 1) {           //On vérifie que la occurrence ne bloque pas la suite de la fonction
+            for (Sentence sentence : text.getContent()) {
+                for (int i = 0; i < sentence.getContent().size() -1; ++i) {
+                    if (randomGenerator.nextFloat() < occurrence) {
+                        swap(sentence.getContent(), i, randomGenerator.nextInt(sentence.getContent().size() - 1));
                     }
                 }
             }
-            return t;
         }
         else
         {
-            for (Sentence s : t) {
-                for (int i = 0; i < s.getContent().size(); ++i) {
-                    List<List<Character>> temp = s.getContent();
-                    swap(temp, i, r.nextInt(temp.size() - 1));
-                    s.setContent(temp);
+            for (Sentence sentence : text.getContent()) {
+                for (int i = 0; i < sentence.getContent().size()-1; ++i) {
+                    swap(sentence.getContent(), i, randomGenerator.nextInt(sentence.getContent().size() - 1));
                 }
             }
-            return t;
         }
     }
 
 
     /**
-     * @param Text t
-     * @param float proba
+     * @param Text text
+     * @param float occurrence
      * @return Text
      */
-    public Text swapContiguousWordsInSentence(Text t, float proba) {
-        Random r = new Random();    //On crée un nombre aléatoire entre 0 et 1
-        if (proba != 1) {           //On vérifie que la proba ne bloque pas la suite de la fonction
-            for (Sentence s : t) {
-                for (int i = 0; i < s.getContent().size(); ++i) {
-                    if (proba > r.nextFloat()) {
-                        List<List<Character>> temp = s.getContent();
-                        swap(temp, i, i  + 1);
-                        s.setContent(temp);
+    public static void swapContiguousWordsInSentence(Text text, float occurrence) {
+        if (occurrence != 1) {           //On vérifie que la occurrence ne bloque pas la suite de la fonction
+            for (Sentence sentence : text.getContent()) {
+                for (int i = 0; i < sentence.getContent().size() -1; ++i) {
+                    if (randomGenerator.nextFloat() < occurrence) {
+                        swap(sentence.getContent(), i, i+1);
                     }
                 }
             }
-            return t;
         } else {
-            for (Sentence s : t) {
-                for (int i = 0; i < s.getContent().size(); ++i) {
-                    List<List<Character>> temp = s.getContent();
-                    swap(temp, i, i  + 1);
-                    s.setContent(temp);
+            for (Sentence sentence : text.getContent()) {
+                for (int i = 0; i < sentence.getContent().size() -1; ++i) {
+                    swap(sentence.getContent(), i, i+1);
                 }
             }
-            return t;
         }
     }
 }
