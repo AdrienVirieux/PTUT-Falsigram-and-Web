d409bf647c98c6fc32b764f1b4cdc950df8a88a3
BorelNathan <nathan.champommier-borel@etu.univ-amu.fr>
2022-03-16 10:09:38 +0100

    j'ai fix l'indentation

--- Springboot_REST_API/src/main/resources/static/JS/script.js
+++ Springboot_REST_API/src/main/resources/static/JS/script.js
@@ -47,73 +47,73 @@ window.onload = function ReadFile(){
 }
 
 function SendRequest(){
-        let url = "http://localhost:8080/RequestJSONProcess";
-        let xhr = new XMLHttpRequest();
-        xhr.open("POST", url);
-        xhr.setRequestHeader("Accept", "application/json")
-        xhr.setRequestHeader("Content-Type", "application/json");
+  let url = "http://localhost:8080/RequestJSONProcess";
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", url);
+  xhr.setRequestHeader("Accept", "application/json")
+  xhr.setRequestHeader("Content-Type", "application/json");
 
-        xhr.onreadystatechange = function () {
-          if (xhr.readyState === 4) {
-            console.log("La réponse du serveur est : " + xhr.responseText);
-            document.querySelector("#paragraphe2").innerHTML = xhr.responseText;
-          }};
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      console.log("La réponse du serveur est : " + xhr.responseText);
+      document.querySelector("#paragraphe2").innerHTML = xhr.responseText;
+    }};
 
-        let Occurence = new Array();
-        let out = document.querySelector("#textinput").value;
-        var nombreInput = document.querySelectorAll(".input").length;
-        for(i = 1; i < nombreInput + 1; i++){
-          let Input = "#Input"+ i;
-          Occurence[i] = document.querySelector(Input).value;
-        }
+  let Occurence = new Array();
+  let out = document.querySelector("#textinput").value;
+  var nombreInput = document.querySelectorAll(".input").length;
+  for(i = 1; i < nombreInput + 1; i++){
+    let Input = "#Input"+ i;
+    Occurence[i] = document.querySelector(Input).value;
+  }
 
-        let wordListInsert = document.querySelector("#ContenueWordListInsert").value;
-        let wordListReplace = document.querySelector("#ContenueWordListReplace").value;
+  let wordListInsert = document.querySelector("#ContenueWordListInsert").value;
+  let wordListReplace = document.querySelector("#ContenueWordListReplace").value;
 
-        console.log(wordListInsert);
-        console.log(wordListReplace);
+  console.log(wordListInsert);
+  console.log(wordListReplace);
 
-        let requete = "{ \"text\" :" + JSON.stringify(out) + ", \"instructions\" : [" +
-        "{\"name\" : \"deleteLetters\", \"occurrence\" : " + Occurence[1] + "}," +
-        "{\"name\" : \"deleteSpaces\", \"occurrence\" : " + Occurence[2] + "}," +
-        "{\"name\" : \"deleteAccents\", \"occurrence\" : " + Occurence[3] + "}," +
-        "{\"name\" : \"deletePunctuations\", \"occurrence\" : " + Occurence[4] + "}," +
-        "{\"name\" : \"insertLetters\", \"occurrence\" : " + Occurence[5] + "}," +
-        "{\"name\" : \"insertSpaces\", \"occurrence\" : " + Occurence[6] + "}," +
-        "{\"name\" : \"insertAccents\", \"occurrence\" : " + Occurence[7] + "}," +
-        "{\"name\" : \"replaceLetters\", \"occurrence\" : " + Occurence[8] + "}," +
-        "{\"name\" : \"replaceAccents\", \"occurrence\" : " + Occurence[9] + "}," +
-        "{\"name\" : \"replacePunctuations\", \"occurrence\" : " + Occurence[10] + "}," +
-        "{\"name\" : \"replaceAHomophones\", \"occurrence\" : " + Occurence[11] + "}," +
-        "{\"name\" : \"replaceEtHomophones\", \"occurrence\" : " + Occurence[12] + "}," +
-        "{\"name\" : \"replaceSaHomophones\", \"occurrence\" : " + Occurence[13] + "}," +
-        "{\"name\" : \"replaceSeHomophones\", \"occurrence\" : " + Occurence[14] + "}," +
-        "{\"name\" : \"replaceSaitHomophones\", \"occurrence\" : " + Occurence[15] + "}," +
-        "{\"name\" : \"replaceDansHomophones\", \"occurrence\" : " + Occurence[16] + "}," +
-        "{\"name\" : \"replaceLaHomophones\", \"occurrence\" : " + Occurence[17] + "}," +
-        "{\"name\" : \"replaceMaisHomophones\", \"occurrence\" : " + Occurence[18] + "}," +
-        "{\"name\" : \"replaceOnHomophones\", \"occurrence\" : " + Occurence[19] + "}," +
-        "{\"name\" : \"replaceOuHomophones\", \"occurrence\" : " + Occurence[20] + "}," +
-        "{\"name\" : \"replacePeuHomophones\", \"occurrence\" : " + Occurence[21] + "}," +
-        "{\"name\" : \"swapLettersInWord\", \"occurrence\" : " + Occurence[22] + "}," +
-        "{\"name\" : \"swapContiguousLettersInWord\", \"occurrence\" : " + Occurence[23] + "}," +
-        "{\"name\" : \"swapWordsInSentence\", \"occurrence\" : " + Occurence[24] + "}," +
-        "{\"name\" : \"swapContiguousWordsInSentence\", \"occurrence\" : " + Occurence[25] + "}," +
-        "{\"name\" : \"insertWordFromSentence\", \"occurrence\" : " + Occurence[26] + "}," +
-        "{\"name\" : \"replaceWordFromSentence\", \"occurrence\" : " + Occurence[27] + "}," +
-        "{\"name\" : \"insertWordsFromList\", \"occurrence\" : " + Occurence[28] + ", \"wordList\" : " +  JSON.stringify(wordListInsert) + "}," +
-        "{\"name\" : \"replaceWordsFromList\", \"occurrence\" : " + Occurence[29] + ", \"wordList\" : " +  JSON.stringify(wordListReplace) + "} ] }";
+  let requete = "{ \"text\" :" + JSON.stringify(out) + ", \"instructions\" : [" +
+  "{\"name\" : \"deleteLetters\", \"occurrence\" : " + Occurence[1] + "}," +
+  "{\"name\" : \"deleteSpaces\", \"occurrence\" : " + Occurence[2] + "}," +
+  "{\"name\" : \"deleteAccents\", \"occurrence\" : " + Occurence[3] + "}," +
+  "{\"name\" : \"deletePunctuations\", \"occurrence\" : " + Occurence[4] + "}," +
+  "{\"name\" : \"insertLetters\", \"occurrence\" : " + Occurence[5] + "}," +
+  "{\"name\" : \"insertSpaces\", \"occurrence\" : " + Occurence[6] + "}," +
+  "{\"name\" : \"insertAccents\", \"occurrence\" : " + Occurence[7] + "}," +
+  "{\"name\" : \"replaceLetters\", \"occurrence\" : " + Occurence[8] + "}," +
+  "{\"name\" : \"replaceAccents\", \"occurrence\" : " + Occurence[9] + "}," +
+  "{\"name\" : \"replacePunctuations\", \"occurrence\" : " + Occurence[10] + "}," +
+  "{\"name\" : \"replaceAHomophones\", \"occurrence\" : " + Occurence[11] + "}," +
+  "{\"name\" : \"replaceEtHomophones\", \"occurrence\" : " + Occurence[12] + "}," +
+  "{\"name\" : \"replaceSaHomophones\", \"occurrence\" : " + Occurence[13] + "}," +
+  "{\"name\" : \"replaceSeHomophones\", \"occurrence\" : " + Occurence[14] + "}," +
+  "{\"name\" : \"replaceSaitHomophones\", \"occurrence\" : " + Occurence[15] + "}," +
+  "{\"name\" : \"replaceDansHomophones\", \"occurrence\" : " + Occurence[16] + "}," +
+  "{\"name\" : \"replaceLaHomophones\", \"occurrence\" : " + Occurence[17] + "}," +
+  "{\"name\" : \"replaceMaisHomophones\", \"occurrence\" : " + Occurence[18] + "}," +
+  "{\"name\" : \"replaceOnHomophones\", \"occurrence\" : " + Occurence[19] + "}," +
+  "{\"name\" : \"replaceOuHomophones\", \"occurrence\" : " + Occurence[20] + "}," +
+  "{\"name\" : \"replacePeuHomophones\", \"occurrence\" : " + Occurence[21] + "}," +
+  "{\"name\" : \"swapLettersInWord\", \"occurrence\" : " + Occurence[22] + "}," +
+  "{\"name\" : \"swapContiguousLettersInWord\", \"occurrence\" : " + Occurence[23] + "}," +
+  "{\"name\" : \"swapWordsInSentence\", \"occurrence\" : " + Occurence[24] + "}," +
+  "{\"name\" : \"swapContiguousWordsInSentence\", \"occurrence\" : " + Occurence[25] + "}," +
+  "{\"name\" : \"insertWordFromSentence\", \"occurrence\" : " + Occurence[26] + "}," +
+  "{\"name\" : \"replaceWordFromSentence\", \"occurrence\" : " + Occurence[27] + "}," +
+  "{\"name\" : \"insertWordsFromList\", \"occurrence\" : " + Occurence[28] + ", \"wordList\" : " +  JSON.stringify(wordListInsert) + "}," +
+  "{\"name\" : \"replaceWordsFromList\", \"occurrence\" : " + Occurence[29] + ", \"wordList\" : " +  JSON.stringify(wordListReplace) + "} ] }";
 
-        console.log("La requete envoyé est : " + requete);
-        xhr.send(requete);
-        document.querySelector("#paragraphe1").innerHTML = requete;
+  console.log("La requete envoyé est : " + requete);
+  xhr.send(requete);
+  document.querySelector("#paragraphe1").innerHTML = requete;
 
-        for(i = 1; i < nombreInput + 1; i++){
-          let inputlabel = "#inputlabel" + i;
-          let label = document.querySelector(inputlabel)
-          let occurence = Occurence[i];
-          label.innerHTML = " Valeure choisie : " + occurence;
-        }
+  for(i = 1; i < nombreInput + 1; i++){
+    let inputlabel = "#inputlabel" + i;
+    let label = document.querySelector(inputlabel)
+    let occurence = Occurence[i];
+    label.innerHTML = " Valeure choisie : " + occurence;
+  }
 }
 
 function updateTextInput(val, id) {
